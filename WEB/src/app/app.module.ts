977e73349d116c1e13d8a64898fdf2c1e6c3e96c
Dario Rodriguez <darior1227@gmail.com>
2020-06-24 22:50:32 -0600

    Reinicio con Archivos de ejemplo

--- WEB/src/app/app.module.ts
+++ WEB/src/app/app.module.ts
@@ -4,24 +4,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { RolesComponent } from './Administrador/roles/roles.component';
-import { SucursalesComponent } from './Administrador/sucursales/sucursales.component';
-import { TrabajadoresComponent } from './Administrador/trabajadores/trabajadores.component';
-import { VendedoresComponent } from './Administrador/vendedores/vendedores.component';
-import { ProductosComponent } from './Administrador/productos/productos.component';
 import { RutasComponent } from './Administrador/rutas/rutas.component';
 import { NavbaraComponent } from './Administrador/navbara/navbara.component';
-import { NavbarcComponent } from './Cliente/navbarc/navbarc.component';
 import { TecboxaComponent } from './Administrador/tecboxa/tecboxa.component';
-import { TecboxcComponent } from './Cliente/tecboxc/tecboxc.component';
-import { RastreopComponent } from './Cliente/rastreop/rastreop.component';
-import { ClientesComponent } from './Cliente/clientes/clientes.component';
-import { CompraproductosComponent } from './Cliente/compraproductos/compraproductos.component';
-import { PaquetesComponent } from './Bodeguero/paquetes/paquetes.component';
-import { RastreoComponent } from './Bodeguero/rastreo/rastreo.component';
-import { NavbarComponent } from './Bodeguero/navbar/navbar.component';
-import { TecboxbComponent } from './Bodeguero/tecboxb/tecboxb.component';
-import { NavbarhComponent } from './shared/navbarh/navbarh.component';
+import { NavbarhComponent } from './home/navbarh/navbarh.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -32,22 +18,8 @@ import { environment } from 'src/environments/environment';
   declarations: [
     AppComponent,
     NavbaraComponent,
-    RolesComponent,
-    SucursalesComponent,
-    TrabajadoresComponent,
-    VendedoresComponent,
-    ProductosComponent,
     RutasComponent,
     TecboxaComponent,
-    NavbarcComponent,
-    TecboxcComponent,
-    RastreopComponent,
-    ClientesComponent,
-    CompraproductosComponent,
-    PaquetesComponent,
-    RastreoComponent,
-    NavbarComponent,
-    TecboxbComponent,
     NavbarhComponent,
     HomeComponent,
     LoginComponent,
